5a0ef50fe50b2b2a40f6583fd9b83b2bd950e9b5
Michael Okoyenta <michaelonyedika32@gmail.com>
2023-01-07 13:57:41 +0100

    Update app_app_2.py

--- app_app_2.py
+++ app_app_2.py
@@ -2,36 +2,36 @@ import streamlit as st
 import pandas as pd
 import pickle
 import numpy as np
+import difflib
 
-st.title('Movie Recommendation System') 
-
-# movies_data = r"C:\Users\CELESTINE TYJC\Desktop\Michael python file\Recommend System\recom_sys_siddhardhan\movies_data.pkl"
-# similarity_file = "https://github.com/michaelonyedika/pt2-Ai-movie-recommendation-system/blob/main/similarity.pkl"
+st.title('Movie Recommendation System')
 
 
 movie_df = pickle.load(open('movies_data.pkl', 'rb'))
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 list_of_movie = np.array(movie_df['title'])
 
-option = st.selectbox(
-    'Select Movie',
-    list_of_movie
-)
+option = st.text_input('Enter Movie Name', placeholder='Movie Name')
+find_match_title = difflib.get_close_matches(option, list_of_movie)
 
 
-def recommend_system(movie):
-    index = movie_df[movie_df['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+def recommend():
+    close_match = find_match_title[0]
+    index_of_title = movie_df[movie_df['title'] == close_match]['index'].values[0]
+
+    similarity_score = list(enumerate(similarity[index_of_title]))
+
+    sorted_similar_movie = sorted(similarity_score, key=lambda x: x[1], reverse=True)
 
     l = []
-    for i in distances[1:6]:
+    for i in sorted_similar_movie[1:6]:
         l.append('{}'.format(movie_df.iloc[i[0]].title))
-
     return l
 
 
-def show_url(movie):
-    index = movie_df[movie_df['title'] == movie].index[0]
+def show_url():
+    close_match = find_match_title[0]
+    index = movie_df[movie_df['title'] == close_match].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x:x[1])
     x = []
     for i in distances[1:6]:
@@ -40,9 +40,18 @@ def show_url(movie):
 
 
 if st.button('Recommend me'):
-    st.write('Movies Recommended for you are:')
-    df = pd.DataFrame({
-        'Movie Recommendation': recommend_system(option),
-        'Movie link': show_url(option)
-    })
-    st.table(df)
+    if len(find_match_title) >= 1:
+        st.write('Movies Recommended for you are:')
+        df = pd.DataFrame({
+            'Movie Recommendation': recommend(),
+            'Movie link': show_url()
+        })
+        st.table(df)
+    else:
+        st.warning('''
+        ### Sorry movie not found, in our database,
+        Enter the correct movie name
+         
+        ''')
+
+st.write('P.S: Thanks to MovieLens, the datasets is from MovieLens, it contains movies from January 09, 1995 to November 21, 2019')
